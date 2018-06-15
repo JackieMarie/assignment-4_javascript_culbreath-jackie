47b820d324ed50c3b28ca917172248f9538a4d11
Jackie Culbreath <jmc081964@gmail.com>
2018-06-15 14:45:51 -0400

    adding arrays

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var iceCream;
+iceCream = ["mint chocolate chip", " salted caramel", " cookies&cream"];
+console.log("My top three favorite ice cream flavors are: ", iceCream);
+
+iceCream.push(" vanilla");
+console.log("My top four favorite ice cream flavors include: ", iceCream);
